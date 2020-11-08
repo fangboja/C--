801e43b79b894463ab3938e460895debb8fdc286
方柏傑 <62335065+fangboja@users.noreply.github.com>
2020-11-08 22:06:36 +0800

    Add files via upload

---
+++ 判斷是否為質數.cpp
@@ -0,0 +1,26 @@
+#include <iostream> //引入 C++ 標頭檔
+#include <stdlib.h> //這一引入標頭檔也可省略，也些編譯器會自動加入
+#include<stdio.h>
+using namespace std;//C++命名空間
+//主函式(main function)
+int main()
+{
+    int num=0;
+	cout<<"請輸入數字";
+    cin>>num;
+    for(int i=2;i<num;i++){
+    	if(num/i==1){
+			cout<<"是質數"<<endl;
+			break; 
+		}
+		else{
+			cout<<"不是質數"<<endl;
+			break; 	
+		}
+		
+		
+	}
+	system("pause");//呼叫內定函式
+    return 0; //返回並傳回0
+}
+
